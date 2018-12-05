293c04de5c45dda2e1b670fe8412e4314b186ff4
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-12-05 07:38:48 +0700

    updated custom-event

--- index.js
+++ index.js
@@ -3,23 +3,18 @@ var regularElements = (function (document) {
 
   /*! (c) Andrea Giammarchi - ISC */
   var self = null || /* istanbul ignore next */ {};
-  try { self.CustomEvent = new CustomEvent('.').constructor; }
-  catch (CustomEvent) {
-    self.CustomEvent = function CustomEvent(type, init) {
-      if (!init)
-        init = {};
-      var bubbles = !!init.bubbles;
-      var cancelable = !!init.cancelable;
-      var e = document.createEvent('Event');
-      e.initEvent(type, bubbles, cancelable);
-      e.detail = init.detail;
-      try {
-        e.bubbles = bubbles;
-        e.cancelable = cancelable;
-      } catch (e) {}
-      return e;
-    };
-  }
+  self.CustomEvent = typeof CustomEvent === 'function' ?
+    CustomEvent :
+    (function (__p__) {
+      CustomEvent[__p__] = new CustomEvent('').constructor[__p__];
+      return CustomEvent;
+      function CustomEvent(type, init) {
+        if (!init) init = {};
+        var e = document.createEvent('CustomEvent');
+        e.initCustomEvent(type, !!init.bubbles, !!init.cancelable, init.detail);
+        return e;
+      }
+    }('prototype'));
   var CustomEvent$1 = self.CustomEvent;
 
   /*! (c) Andrea Giammarchi - ISC */
